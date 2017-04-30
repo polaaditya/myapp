f5898b01160686b3e6fac6b5f015f12d51a88d68
poladinesh <dp.websphere@gmail.com>
2017-04-30 15:55:22 -0400

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'myapp'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures myapp'
+long_description 'Installs/Configures myapp'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/myapp/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/myapp' if respond_to?(:source_url)
